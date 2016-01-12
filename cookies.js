043d71bddc88d36c96aad7e6db9147df45eee8a8
JGallegos1 <JGallegos@sfo.yearup.org>
2016-01-11 21:23:00 -0800

    Added headers to every page and created a login function

--- cookies.js
+++ cookies.js
@@ -1,52 +1,66 @@
-// ***********************  COOKIE FUNCTIONS, INCLUDED ON ALL PAGES  ********************
+function createCookie(name,value,email) {
 
-// Create cookie for the first time
-function createCookie(name,value) {
-	// basic error checking - name cannot be blank
 	if (value.length < 1) {
 		alert("Please enter your name to login");
-		return false;
+		return false
 		}
-	// ***** perform additional error checking here 
-	
-	document.cookie = name+"="+value+"; path=/";
-	location.reload();   // reloading the page causes the cookie and logout link to be displayed on the page
+
+
+	var status = false;     
+	var emailRegEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+	     if (document.frmLogin.email.value.search(emailRegEx) == -1) {
+	          alert("Please enter a valid email address.");
+	     }
+	     else {
+	          alert("Thank you for logging in.");
+	          window.location.reload();
+	          status = true;
+	     }
+	     return document.cookie = name+"="+value+"; path=/";
 }
 
-// read the cookie, if it exists, and display on the page
+
+
 function readCookie(name) {
-	var nameEQ = name + "=";  // this will equal the first part of the cookie string
+	var nameEQ = name + "=";  
 	var visitorName = "";
-	var cookieList = document.cookie.split(';');   // get the cookie string as an array, and split on ; character
+	var cookieList = document.cookie.split(';');   
 	for(var i=0;i < cookieList.length;i++) {
 		var c = cookieList[i];
 		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) {       // find the cookie that begins with "myCookie" 
-			visitorName = c.substring(nameEQ.length,c.length)  // get whatever is to the right of the = sign
+		if (c.indexOf(nameEQ) == 0) { 
+			visitorName = c.substring(nameEQ.length,c.length)  
 		}
 	}
 	
-	//determine if visitor is logged in, or not, and display appropriate text
 	if (visitorName.length > 0) {
-		var writeLink = "Welcome " + visitorName + " - <a href=\"javascript:eraseCookie('myCookie')\">Log Out</a>"
+		var writeLink = "Hello " + visitorName + " - <a href=\"javascript:eraseCookie('myCookie');eraseLogin();eraseEmail();\">Logout</a>"
 	} 
 	else
 	{
-		var writeLink = "Hello. Please log in." // cookie is empty, so don't add any text to the page
+		var writeLink = "Welcome, Please Log In" 
 	}
 	 
-	document.write(writeLink);  // this line causes the cookie, plus some text, to be added to the page dynamically
+	document.write(writeLink);
 }
 
-// Delete the cookie and reload the page to clear it
 function eraseCookie(name) {
-	document.cookie = name+"=;expires='1/1/2000'; path=/"; //setting to past date causes it to expire
-	document.forms["frmLogin"].fname.value=''; location.reload(); // on your web page, delete this line and un-comment the one below
-	// location.href='page1.html'  // replace this with your default home page, if different than this
+	document.cookie = name+"=;expires='1/1/2000'; path=/";
+
+		alert("Thank you for logging out.");
+		          window.location.reload();
+		          status = true; 
 }
 
-// Use this just to do a quick check if we've set our cookie properly
-// Remove the 'Show Cookies' button from the page after debugging
+function eraseLogin(name) {
+	document.forms["frmLogin"].fname.value=''; location.reload(); 
+}
+
+function eraseEmail(email) {
+	document.forms["frmLogin"].email.value=''; location.reload(); 
+}
+
+
 function showCookie() {
 	alert(document.cookie)
 }
